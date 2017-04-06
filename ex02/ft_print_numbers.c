7f55b5174475726ae8cd0882c814f01c32a3fe9c
Wes Johnson <wes@futureprefect.com>
2017-04-05 19:54:57 -0700

    Fix style errors

--- ex02/ft_print_numbers.c
+++ ex02/ft_print_numbers.c
@@ -6,22 +6,22 @@
 /*   By: wjohnson <wjohnson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/05 19:27:32 by wjohnson          #+#    #+#             */
-/*   Updated: 2017/04/05 19:47:27 by wjohnson         ###   ########.fr       */
+/*   Updated: 2017/04/05 19:54:35 by wjohnson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 void	ft_print_numbers(void)
 {
 	int i;
-	int asciiOne;
-	int asciiNine;
+	int ascii_one;
+	int ascii_nine;
 	int lf;
 
-	asciiOne = 48;
-	asciiNine = 57;
+	ascii_one = 48;
+	ascii_nine = 57;
 	lf = 10;
-	i = asciiOne;
-	while (i<=asciiNine)
+	i = ascii_one;
+	while (i <= ascii_nine)
 	{
 		ft_putchar(i);
 		i++;
