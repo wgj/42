c6bd20081aeea96973dfd6035bba8cd54adc97d0
Wes Johnson <wes@futureprefect.com>
2017-04-08 02:43:48 -0700

    Norminette correction

--- ex00/ft_iterative_factorial.c
+++ ex00/ft_iterative_factorial.c
@@ -6,11 +6,12 @@
 /*   By: wjohnson <wjohnson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/08 02:29:07 by wjohnson          #+#    #+#             */
-/*   Updated: 2017/04/08 02:35:50 by wjohnson         ###   ########.fr       */
+/*   Updated: 2017/04/08 02:43:32 by wjohnson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int	ft_iterative_factorial(int nb) {
+int	ft_iterative_factorial(int nb)
+{
 	int i;
 	int ret;
 
