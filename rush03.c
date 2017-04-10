a535fbb0b4c427fb430dd66a9bbcdddb2df481ca
Wes Johnson <wes@futureprefect.com>
2017-04-09 18:14:06 -0700

    Wes > Norm

--- rush03.c
+++ rush03.c
@@ -6,7 +6,7 @@
 /*   By: wjohnson <wjohnson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/08 14:46:10 by wjohnson          #+#    #+#             */
-/*   Updated: 2017/04/09 17:07:21 by wjohnson         ###   ########.fr       */
+/*   Updated: 2017/04/09 18:10:44 by wjohnson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,67 +24,32 @@ int		ft_putchar(char c);
 ** Print() prints elements of Rush()'s array,
 ** and contains the logic for the look of the array.
 */
+
 void	print(int xpos, int ypos, int x, int y)
 {
 	char	c;
-	char	top;
-	char	left;
-	char	center;
-	char	right;
-	char	bottom;
-	char	top_left_corner;
-	char	top_right_corner;
-	char	bottom_left_corner;
-	char	bottom_right_corner;
+	char	wall;
+	char	left_corner;
+	char	right_corner;
 
-	top = 'B';
-	left = 'B';
-	center = ' ';
-	right = 'B';
-	bottom = 'B';
-	top_left_corner = 'A';
-	top_right_corner = 'C';
-	bottom_left_corner = 'A';
-	bottom_right_corner = 'C';
-	if ((xpos > 1 && xpos < x) && ypos == 1)
+	c = ' ';
+	wall = 'B';
+	left_corner = 'A';
+	right_corner = 'C';
+	if (((xpos > 1 && xpos < x) && ypos == 1) || \
+		(xpos == 1 && (ypos > 1 && ypos < y)) || \
+		(xpos == x && (ypos > 1 && ypos < y)) || \
+		((xpos > 1 && xpos < x) && ypos == y))
 	{
-		c = top;
+		c = wall;
 	}
-	if (xpos == 1 && (ypos > 1 && ypos < y))
+	if ((xpos == 1 && ypos == 1) || (xpos == 1 && ypos == y))
 	{
-		c = left;
+		c = left_corner;
 	}
-	if ((xpos > 1 && xpos < x) && (ypos > 1 && ypos < y))
+	if ((x > 1 && ((xpos == x && ypos == 1) || (xpos == x && ypos == y))))
 	{
-		c = center;
-	}
-	if (xpos == x && (ypos > 1 && ypos < y))
-	{
-		c = left;
-	}
-	if ((xpos > 1 && xpos < x) && ypos == y)
-	{
-		c = bottom;
-	}
-	if (xpos == 1 && ypos == 1)
-	{
-		c = top_left_corner;
-	}
-	if (x > 1 && (xpos == x && ypos == 1))
-	{
-		c = top_right_corner;
-	}
-	if (xpos == 1 && ypos == y)
-	{
-		c = bottom_left_corner;
-	}
-	if (x > 1 && (xpos == x && ypos == y))
-	{
-		c = bottom_right_corner;
-	}
-	if (xpos > x)
-	{
-		c = '\n';
+		c = right_corner;
 	}
 	ft_putchar(c);
 }
@@ -99,10 +64,6 @@ int		rush(int x, int y)
 	int		xpos;
 	int		ypos;
 
-	if (x == 0 || y == 0)
-	{
-		return (-1);
-	}
 	xpos = 1;
 	ypos = 1;
 	while (ypos <= y)
@@ -112,7 +73,14 @@ int		rush(int x, int y)
 			print(xpos, ypos, x, y);
 			xpos++;
 		}
-		print(xpos, ypos, x, y);
+		if (xpos > x)
+		{
+			ft_putchar('\n');
+		}
+		else
+		{
+			print(xpos, ypos, x, y);
+		}
 		xpos = 1;
 		ypos++;
 	}
