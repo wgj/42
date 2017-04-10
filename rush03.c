e5f1f29d7883fda8b50336a3d0ae2bbdee5418eb
Wes Johnson <wes@futureprefect.com>
2017-04-09 21:48:42 -0700

    Improve function comments

--- rush03.c
+++ rush03.c
@@ -6,7 +6,7 @@
 /*   By: wjohnson <wjohnson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/08 14:46:10 by wjohnson          #+#    #+#             */
-/*   Updated: 2017/04/09 18:10:44 by wjohnson         ###   ########.fr       */
+/*   Updated: 2017/04/09 21:48:10 by wjohnson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,8 +21,13 @@
 int		ft_putchar(char c);
 
 /*
-** Print() prints elements of Rush()'s array,
-** and contains the logic for the look of the array.
+** Print() prints elements of rush()'s array, and contains the logic for the
+** look of the array. Print() is composed to several if statements checking for
+** edges of the arithmetic array x by y. If all tests fail, the 'point'
+** described by xpos and ypos is assumed to be a non-edge, and variable c will
+** 'fallthrough' with it's initial declaration (' ').
+** Parameters x and y maybe better suited as global variables, particularly if
+** modifications are needed.
 */
 
 void	print(int xpos, int ypos, int x, int y)
@@ -55,8 +60,10 @@ void	print(int xpos, int ypos, int x, int y)
 }
 
 /*
-** Rush() will loop through an array of x by y,
-** calling print() at every position.
+** Rush() will loop through an array of x by y, calling print() at every
+** position. x and y are invariant representating an x by y array similar to
+** quadrant 1 of a cartesian graph. xpos and ypos represent the current
+** 'point' on the graph, and are used mainly for calls to print().
 */
 
 int		rush(int x, int y)
