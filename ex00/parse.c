581465e4c5b1b58bee21c7fe0d55a36c34a93adb
Logan James Kaser <lkaser@e1z1r3p42.42.us.org>
2017-04-16 14:50:15 -0700

    ADD: get_empty_square(), example of simple print unt pharse.c main.

--- ex00/parse.c
+++ ex00/parse.c
@@ -6,7 +6,7 @@
 /*   By: lkaser <lkaser@student.42.us.org>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/15 17:25:06 by lkaser            #+#    #+#             */
-/*   Updated: 2017/04/15 21:43:15 by lkaser           ###   ########.fr       */
+/*   Updated: 2017/04/16 14:37:50 by lkaser           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,9 +22,16 @@ int		len(char *str)
 	return (len);
 }
 
+int		is_valid(char **grid)
+{
+	(void)grid;
+	return (1);
+}
+
 char	**parse_arguments(int argc, char **argv)
 {
-	short int char_count;
+	int char_count;
+	char **grid;
 
 	if (argc != 10)
 		return (NULL);
@@ -33,7 +40,16 @@ char	**parse_arguments(int argc, char **argv)
 		char_count += len(argv[argc]);
 	if (char_count != 81)
 		return (NULL);
-	return (++argv);
+
+	++argv;
+	grid = malloc(sizeof(char*) * 10);
+	argc = -1;
+	while (++argc < 9)
+		grid[argc] = argv[argc];
+	grid[9] = 0;
+	if (!is_valid(grid))
+		return (NULL);
+	return (grid);
 }
 
 int		main(int argc, char **argv)
@@ -43,5 +59,11 @@ int		main(int argc, char **argv)
 	grid = parse_arguments(argc, argv);
 	if (grid == NULL)
 		write(1, "Error\n", 6);
+	while (*grid)
+	{
+		write(1, *grid, 9);
+		write(1, "\n", 1);
+		++grid;
+	}
 	return (0);
 }
