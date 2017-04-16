2c88cdb8246ee624ddff5b806a6f86929a40ef77
Wes Johnson <wes@futureprefect.com>
2017-04-16 13:59:13 -0700

    Add code review comments and TODOs

--- ex00/checks.c
+++ ex00/checks.c
@@ -6,7 +6,7 @@
 /*   By: wjohnson <wjohnson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/15 18:56:06 by wjohnson          #+#    #+#             */
-/*   Updated: 2017/04/16 12:26:36 by wjohnson         ###   ########.fr       */
+/*   Updated: 2017/04/16 13:57:42 by wjohnson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,9 +41,10 @@ int	is_vunit_valid(char square, int row, int col, char **grid)
 	return (1);
 }
 
-int	is_hunit_valid(char square, int row, int col, char **grid)
+int	is_hunit_valid(char square, int row, int col, char **grid) 
 {
 	// i am guessing the row, col is the current coordinate of square?
+	// It's the current coordinate of a "potential" square. If the tests pass, then it's safe to put `char square` into `grid[x][y]`. -wgj
 	int i;
 	char peer;
 
@@ -53,9 +54,11 @@ int	is_hunit_valid(char square, int row, int col, char **grid)
 		peer = grid[i][col];
 		if (peer < '0' || peer > '9')
 			return (0); //this should probably return an exit error
+						// Agreed, but I think that's up to the context of the caller of  is_valid(). -wgj
 		if (square == peer)
 			return (0);
 		//why do we return false if peer is not empty? we should probably just ignore it?
+		// Well, it depends if is_valid() is to check for a potential square, or an existing square. Let's talk about it together. -wgj
 		i++;
 	}
 	return (1);
@@ -67,7 +70,7 @@ int	is_bunit_valid(char square, int row, int col, char **grid)
 
 	corner_row = 0;
 	corner_col = 0; //i can probably move this to a diff function...
-	if (row <= 2)
+	if (row <= 2) // I think corner_row and corner_col are creating a box around grid[row][col], instead of defining the box grid[x][y] is in (I could be wrong). Let's talk about it -wrong :)
 		corner_row = 0;
 	if (row <= 5)
 		corner_row = 3;
@@ -91,7 +94,7 @@ int	is_bunit_valid(char square, int row, int col, char **grid)
 		j = corner_col;
 		while (j < corner_col + 3)
 		{
-			if (grid[i][j] == square && i != row && j != col)
+			if (grid[i][j] == square && i != row && j != col) // In the above functions, I was making the presumption that grid[row][col] will be empty, so it's safe to check. This is another example where we need to figure out if our checks should be "before writing a sqaure" or "after writing a square". -wgj
 				return (0);
 			j++;
 		}
@@ -107,8 +110,12 @@ int	is_bunit_valid(char square, int row, int col, char **grid)
 */
 int	is_valid(char square, int row, int col, char **grid)
 {
-	// TODO check that row and rol are valid input.
+	// TODO swap row and col
+	// TODO rename row and col to x y, respectively.
+	// TODO check that row and rol are valid input. Though maybe this should be done on parse?
 	// TODO check that square is valid input.
-	// TODO figure out a way to figure is a valid input.
+	// TODO figure out a way to see grid a valid input.
+	// TODO add function declaration to sudoku.h
+	// TODO add precheck that grid[row][col] is empty, return false.
 	return (is_vunit_valid(square, row, col, grid) && is_hunit_valid(square, row, col, grid) && is_bunit_valid(square, row, col, grid));
 }
