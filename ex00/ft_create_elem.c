51f79d286f199e83b0fe398fb8764c347d28c58c
Wes Johnson <wes@futureprefect.com>
2017-04-18 16:00:01 -0700

    Solution for ex00

---
+++ ex00/ft_create_elem.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_create_elem.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wjohnson <wjohnson@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/18 14:36:57 by wjohnson          #+#    #+#             */
+/*   Updated: 2017/04/18 15:34:11 by wjohnson         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+#include <stdlib.h>
+
+t_list	*ft_create_elem(void *data)
+{
+	t_list *list;
+	list = malloc(sizeof(t_list));
+	list->data = data;
+	list->next = NULL;
+	return list;
+}
