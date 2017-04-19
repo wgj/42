a864f80d0c39aa30be0d0d82cdccda02b1e63a1a
Wes Johnson <wes@futureprefect.com>
2017-04-18 23:35:12 -0700

    Solution for ex01

---
+++ ex01/ft_list_push_back.c
@@ -0,0 +1,29 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_list_push_back.c                                :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wjohnson <wjohnson@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2017/04/18 16:09:05 by wjohnson          #+#    #+#             */
+/*   Updated: 2017/04/18 23:32:08 by wjohnson         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_list.h"
+
+void	ft_list_push_back(t_list **begin_list, void *data)
+{
+	t_list *list;
+
+	list = *begin_list;
+	if (!list)
+	{
+		list = ft_create_elem(data);
+	}
+	while (list->next)
+	{
+		list = list->next;
+	}
+	list->next = ft_create_elem(data);
+}
