4e87e1728124718b728d0d040bd954665a5bffc7
Wes Johnson <wes@futureprefect.com>
2017-04-26 23:39:38 -0700

    Fix norminette

--- isvalid.c
+++ isvalid.c
@@ -6,19 +6,18 @@
 /*   By: wjohnson <wjohnson@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2017/04/26 23:23:27 by wjohnson          #+#    #+#             */
-/*   Updated: 2017/04/26 23:23:35 by wjohnson         ###   ########.fr       */
+/*   Updated: 2017/04/26 23:39:28 by wjohnson         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
 #include "header.h"
 
-int	checknextsize(int *coor, int size, char **field, int *info)
+int		checknextsize(int *coor, int size, char **field, int *info)
 {
 	int i;
 
 	i = 0;
-	while(i < size + 1)
+	while (i < size + 1)
 	{
 		if (coor[0] + size > info[0] || coor[1] + size > info[4])
 			return (0);
@@ -31,23 +30,23 @@ int	checknextsize(int *coor, int size, char **field, int *info)
 	return (checknextsize(coor, size + 1, field, info) + 1);
 }
 
-int     isvalid(int *coor, int size, char **field, int *info)
+int		isvalid(int *coor, int size, char **field, int *info)
 {
 	int i;
 	int j;
 
 	i = 0;
 	j = 0;
-	while(j < size)
+	while (j < size)
 	{
-		while(i < size)
+		while (i < size)
 		{
-			if(field[coor[0] + i][coor[1] + j] == info[2])
+			if (field[coor[0] + i][coor[1] + j] == info[2])
 				return (-i);
-		i++;
+			i++;
 		}
-	i = 0;
-	j++;
+		i = 0;
+		j++;
 	}
 	return (checknextsize(coor, size, field, info) + 1);
 }
